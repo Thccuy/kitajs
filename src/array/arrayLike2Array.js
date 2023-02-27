8d50b26201f665ffc4b22007a2da2c73178f9961
hctang <hctang@zjft.com>
2023-02-27 10:27:26 +0800

    add arrayLike2Array.js

---
+++ src/array/arrayLike2Array.js
@@ -0,0 +1,19 @@
+import isArrayLike from "../utils/isArrayLike";
+
+function arrayLike2Array(arrLike) {
+    const res = [];
+
+    if(Array.isArray(arrLike)) {
+        return arrLike;
+    }
+
+    if(isArrayLike(arrLike)) {
+        for (let i = 0; i < arrLike.length; i++) {
+            res[i] = arrLike[i];
+        }
+    }else {
+        throw new Error(`bad parameter type: ${typeof arrLike}`)
+    }
+
+    return res;
+}
