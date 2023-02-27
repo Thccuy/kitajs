9056bd36d5934a224c768442be587e33ca09e656
hctang <hctang@zjft.com>
2023-02-27 08:55:08 +0800

    add debounce.js

---
+++ src/function/debounce.js
@@ -0,0 +1,11 @@
+function debounce(fn,timeout = 0) {
+    let timer = null;
+
+    return (...args) => {
+        if (timer) {
+            clearTimeout(timer);
+        }
+
+        timer = setTimeout(fn.apply(this,args));
+    }
+}
